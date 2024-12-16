0506673f69473aa4e5ebf4ee83798c8234a1d255
Brian <chumobrian@gmail.com>
2024-12-16 23:38:40 +0300

    feat: add dashboard main script to render dashboard content and handle user interactions

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+
+$('body').append('<p>ALX Dashboard</p>')
+$('body').append('<p>Dashboard data for the students</p>')
+$('body').append('<button id="click-button">Click here to get started</button>')
+$('body').append("<p id='count'></p>")
+$('body').append('<p>Copyright - ALX</p>')
+
+var count = 0;
+
+const updateCounter = () => {
+	count += 1;
+	$('#count').text(`${count} clicks on the button`);
+}
+
+jQuery('#click-button').on('click', _.debounce(updateCounter, 300));
